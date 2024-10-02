17ac8c551ca2e05d89dd083736b0ac769aca0a81
Uday vara <varaudayd@gmail.com>
2024-10-03 00:49:25 +0530

    added Category Module Res

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { CommonModule } from './common/common.module';
 import { AuthModule } from './auth/auth.module';
 import { EnviromentModule } from './enviroment/enviroment.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [CommonModule, AuthModule, EnviromentModule],
+  imports: [CommonModule, AuthModule, EnviromentModule, CategoryModule],
   controllers: [AppController],
   providers: [AppService],
   exports: [CommonModule],
