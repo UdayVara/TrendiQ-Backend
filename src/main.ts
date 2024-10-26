07e6be18c8a55831c1fcf0d8b62038b7ad37aeab
Uday vara <varaudayd@gmail.com>
2024-10-27 00:11:22 +0530

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,14 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { HttpExceptionFilter } from './Filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api/v1');
-  app.useGlobalPipes(new ValidationPipe({
-    transform:true
-  }))
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   app.useGlobalFilters(new HttpExceptionFilter());
   const config = new DocumentBuilder()
     .setTitle('TrendiQ')
@@ -21,7 +22,7 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/documentation', app, document);
-
+  app.enableCors();
   await app.listen(5643);
 }
 bootstrap();
