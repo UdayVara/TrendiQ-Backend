e7f83d91a02a98f51871e8fd74616236eacab768
Uday vara <varaudayd@gmail.com>
2024-10-28 00:24:54 +0530

    Added Description and examples for swagger response

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,49 @@
-import {  Body, Controller, Post} from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AdminSignupDto } from './dto/admin-signup.dto';
 import { AdminSigninDto } from './dto/admin-signin.dto';
+import { ApiBody, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-
-  @Post("/admin/signin")
+  @Post('/admin/signin')
+  @ApiBody({
+    description: 'This API Endpoint is used to sign in with bearer auth.',
+    schema: {
+      type: 'object',
+      properties: { email: { type: 'string' }, password: { type: 'string' } },
+    },
+  })
+  @ApiCreatedResponse({
+    example: {
+      statusCode: 201,
+      message: 'Admin Signin Successfully.',
+      user: {},
+    },
+  })
   async login(@Body() adminSigninBody: AdminSigninDto) {
-    return this.authService.adminSignin(adminSigninBody)
+    return this.authService.adminSignin(adminSigninBody);
   }
 
-
-  @Post("/admin/signup")
+  @Post('/admin/signup')
+  @ApiBody({
+    description: 'This API Endpoint is used to sign Up with bearer auth.',
+    schema: {
+      type: 'object',
+      properties: { email: { type: 'string' }, password: { type: 'string' } },
+    },
+  })
+  @ApiCreatedResponse({
+    example: {
+      statusCode: 201,
+      message: 'Admin Signup Successfully.',
+      user: {},
+    },
+  })
   async signUp(@Body() adminSignupBody: AdminSignupDto) {
-    return this.authService.adminSignup(adminSignupBody)
+    return this.authService.adminSignup(adminSignupBody);
   }
 }
--- src/auth/dto/admin-signin.dto.ts
+++ src/auth/dto/admin-signin.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class AdminSigninDto {
+  @ApiProperty({type:"string",example:"PjG2z@example.com"})
   @IsEmail()
   email: string;
 
+  @ApiProperty({type:"string",example:"Test@1234"})
   @IsNotEmpty()
   password: string;
 
